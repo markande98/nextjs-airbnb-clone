78ffa647680933c2752761796de45efdfb9fcfe6
markande98 <gauravbpit1998@gmail.com>
2023-09-19 12:18:41 +0530

    navbar completed

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,8 +1,16 @@
 "use client";
 
-import { AiOutlineMenu } from 'react-icons/ai';
-import Avatar from '../Avatar';
+import { AiOutlineMenu } from "react-icons/ai";
+import Avatar from "../Avatar";
+import { useCallback, useState } from "react";
+import MenuItem from "./MenuItem";
 const UserMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = useCallback(() => {
+      setIsOpen((value) => !value);
+    }, []);
+
   return (
     <div className="relative">
       <div className="flex items-center gap-x-3">
@@ -21,11 +29,11 @@ const UserMenu = () => {
                         cursor-pointer
                     "
         >
-            Airbnb you home
+          Airbnb you home
         </div>
         <div
-            onClick={() => {}}
-            className="
+          onClick={toggleOpen}
+          className="
                 p-4
                 md:py-1
                 md:px-2
@@ -40,12 +48,35 @@ const UserMenu = () => {
                 transition
             "
         >
-            <AiOutlineMenu />
-            <div className='hidden md:block'>
-                <Avatar />
-            </div>
+          <AiOutlineMenu />
+          <div className="hidden md:block">
+            <Avatar />
+          </div>
         </div>
       </div>
+      {isOpen && (
+        <div
+          className="
+            rounded-xl
+            absolute
+            shadow-md
+            w-[40vw]
+            md:w-3/4
+            bg-white
+            overflow-hidden
+            right-0
+            top-12
+            text-sm
+        "
+        >
+          <div className="flex flex-col cursor-pointer">
+            <>
+              <MenuItem label="login" onClick={() => {}} />
+              <MenuItem label="Sign up" onClick={() => {}} />
+            </>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
