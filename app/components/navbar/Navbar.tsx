f1eec15d0b09966a69cdc7daafa338f93f96dac8
markande98 <gauravbpit1998@gmail.com>
2023-09-19 11:30:17 +0530

    project setup & cleaning

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Container from "../Container";
+
+const Navbar = () => {
+  return (
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div className="p-4 border-b-[1px]">
+        <Container>hello</Container>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
