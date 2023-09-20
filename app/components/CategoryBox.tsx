6c6917cbfb4509fc739db8d3028eaf22bb5818e0
markande98 <gauravbpit1998@gmail.com>
2023-09-20 12:39:38 +0530

    category item completed

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -1,4 +1,8 @@
+import { useRouter, useSearchParams } from "next/navigation";
+import { useCallback } from "react";
 import { IconType } from "react-icons";
+import qs from "query-string";
+import { skip } from "node:test";
 
 interface CategoryBoxProps {
   icon: IconType;
@@ -11,8 +15,38 @@ const CategoryBox: React.FC<CategoryBoxProps> = ({
   label,
   selected,
 }) => {
+  const router = useRouter();
+  const params = useSearchParams();
+
+  const handleClick = useCallback(() => {
+    let currentQuery = {};
+
+    if (params) {
+      currentQuery = qs.parse(params.toString());
+    }
+
+    const updatedQuery: any = {
+      ...currentQuery,
+      category: label,
+    };
+
+    if (params?.get("category") === label) {
+      delete updatedQuery.category;
+    }
+
+    const url = qs.stringifyUrl(
+      {
+        url: "/",
+        query: updatedQuery,
+      },
+      { skipNull: true }
+    );
+
+    router.push(url);
+  }, [label, params, router]);
   return (
     <div
+      onClick={handleClick}
       className={`
         flex
         flex-col
