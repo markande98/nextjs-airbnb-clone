0e8291a26c6282b2068596f866475337e9ea2357
markande98 <gauravbpit1998@gmail.com>
2023-09-21 17:43:53 +0530

    listing page completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import getCurrentUser from "./actions/getCurrentUser";
-import getListings from "./actions/getListings";
+import getListings, { IListingsParams } from "./actions/getListings";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/listings/ListingCard";
 
-export default async function Home() {
-  const listings = await getListings();
+interface HomeProps {
+  serachParams: IListingsParams;
+}
+
+const Home = async ({ serachParams }: HomeProps) => {
+  const listings = await getListings(serachParams);
   const currentUser = await getCurrentUser();
 
   if (listings.length === 0) return <EmptyState showReset />;
@@ -24,4 +28,6 @@ export default async function Home() {
       </div>
     </Container>
   );
-}
+};
+
+export default Home;
