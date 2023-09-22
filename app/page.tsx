34d5e51d59d57bc7caeca3205f66f01d3102e932
markande98 <gauravbpit1998@gmail.com>
2023-09-22 07:44:21 +0530

    added loader or error page

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,7 @@ const Home = async ({ searchParams }: HomeProps) => {
   const currentUser = await getCurrentUser();
 
   if (listings.length === 0) return <EmptyState showReset />;
+
   return (
     <Container>
       <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
